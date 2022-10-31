2ee3fff7c3c046509bad8b2a30aadde4ec604857
Daniel Mateu <danielmateu86@gmail.com>
2022-10-31 19:12:44 +0100

    Mostrando usuarios desde admin role

--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -172,7 +172,10 @@ export const SideMenu = () => {
                                     <ListItemText primary={'Pedidos'} />
                                 </ListItem>
 
-                                <ListItem button>
+                                <ListItem 
+                                    button
+                                    onClick={() => navigateTo(`/admin/users`)}
+                                >
                                     <ListItemIcon>
                                         <AdminPanelSettings />
                                     </ListItemIcon>
