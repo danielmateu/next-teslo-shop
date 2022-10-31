70f6b4f8b7208ba7e88f37d96cc1eece3f6b0772
Daniel Mateu <danielmateu86@gmail.com>
2022-10-31 16:54:07 +0100

    More small changes

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -4,4 +4,5 @@ export * from './cart';
 export * from './order';
 export * from './products';
 export * from './user';
+export * from './dashboard';
 export * as IPaypal from './paypal';
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,10 +1,52 @@
-import { AccessTimeOutlined, AttachmentOutlined, AttachMoney, CancelPresentationOutlined, CategoryOutlined, CreditCard, CreditCardOffOutlined, CreditCardOutlined, DashboardOutlined, GroupAddOutlined, GroupsOutlined, ProductionQuantityLimitsOutlined } from '@mui/icons-material'
-import { Card, CardContent, Grid, Typography } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import useSWR from 'swr';
+import { DashboardSummaryResponse } from '../../interfaces';
+
+import { AccessTimeOutlined , AttachMoney, CancelPresentationOutlined, CategoryOutlined, CreditCardOffOutlined, CreditCardOutlined, DashboardOutlined, GroupsOutlined, ProductionQuantityLimitsOutlined } from '@mui/icons-material'
+import { Grid, Typography } from '@mui/material'
+
 import { AdminLayout } from '../../components/layouts'
 import { SummaryTile } from '../../components/admin/SummaryTile';
+import { FullScreenLoading } from '../../components/ui';
 
-const DashboardPage = () => {
+export const DashboardPage = () => {
+
+    const {data,error} = useSWR<DashboardSummaryResponse>('api/admin/dashboard', {
+        refreshInterval: 30 * 1000
+    });
+
+    const [refreshIn, setRefreshIn] = useState(30);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setRefreshIn(refreshIn=> refreshIn > 0 ? refreshIn - 1 : 30);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    if(!error && !data){
+        return (
+            <FullScreenLoading/>
+        )
+    }
+
+    if(error){
+        console.log(error);
+        return <Typography>Error al cargar la información</Typography>
+    }
+
+    const { 
+        numberOfOrders,
+        paidOrders,
+        notPaidOrders,
+        numberOfClients,
+        numberOfProducts,
+        productsWithNoInventory,
+        lowInventory,
+    } = data!;
+    
+
     return (
         <AdminLayout
             title={'Dashboard'}
@@ -12,47 +54,45 @@ const DashboardPage = () => {
             icon={<DashboardOutlined />}>
             <Grid container spacing={2}>
 
-
-
                 <SummaryTile
-                    title={'1'}
+                    title={numberOfOrders}
                     subtitle={'Pedidos Totales'}
                     icon={<CreditCardOutlined color='secondary' sx={{ fontSize: 40 }} />}
                 />
 
                 <SummaryTile
-                    title={'2'}
+                    title={paidOrders}
                     subtitle={'Pedidos Pagados'}
                     icon={<AttachMoney color='success' sx={{ fontSize: 40 }} />}
                 />
 
                 <SummaryTile
-                    title={'3'}
+                    title={notPaidOrders}
                     subtitle={'Pedidos Pendientes'}
                     icon={<CreditCardOffOutlined color='error' sx={{ fontSize: 40 }} />}
                 />
                 <SummaryTile
-                    title={'4'}
+                    title={numberOfClients}
                     subtitle={'Clientes'}
                     icon={<GroupsOutlined color='primary' sx={{ fontSize: 40 }} />}
                 />
                 <SummaryTile
-                    title={'5'}
+                    title={numberOfProducts}
                     subtitle={'Productos'}
                     icon={<CategoryOutlined color='warning' sx={{ fontSize: 40 }} />}
                 />
                 <SummaryTile
-                    title={'6'}
+                    title={productsWithNoInventory}
                     subtitle={'Sin Stock'}
                     icon={<CancelPresentationOutlined color='error' sx={{ fontSize: 40 }} />}
                 />
                 <SummaryTile
-                    title={'7'}
+                    title={lowInventory}
                     subtitle={'Bajo Stock'}
                     icon={<ProductionQuantityLimitsOutlined color='warning' sx={{ fontSize: 40 }} />}
                 />
                 <SummaryTile
-                    title={'8'}
+                    title={refreshIn}
                     subtitle={'Actualización en:'}
                     icon={<AccessTimeOutlined color='secondary' sx={{ fontSize: 40 }} />}
                 />
